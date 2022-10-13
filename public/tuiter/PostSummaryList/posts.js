62b15a5055f959d47bea85b868b819d73086dc1d
adriannaliu <72051204+adriannaliu@users.noreply.github.com>
2022-10-13 01:31:03 -0400

    implemented tuiter with jquery, few things to refactor

---
+++ public/tuiter/PostSummaryList/posts.js
@@ -0,0 +1,31 @@
+export default [
+  {
+    topic: "Web Development",
+    userName: "ReactJS",
+    time: "2h",
+    image: "../../images/javascript.png",
+    title: "React.js is a component based front end library that makes it very easy to build Single Page Applications or SPAs",
+  },
+  {
+    topic: "",
+    userName: "JavaScript",
+    time: "1 day",
+    image: "../../images/react.svg",
+    title: "JavaScript is a programming language that can run on browsers as well as desktops",
+  },
+  {
+    topic: "Web Development",
+    userName: "",
+    time: "last week",
+    image: "../../images/jquery.png",
+    title: "",
+  },
+  {
+    topic: "Web Development",
+    userName: "NodeJS",
+    time: "last month",
+    image: "../../images/nodejs.png",
+    title: "",
+  },
+];
+
---
+++ public/tuiter/WhoToFollowList/whos.js
@@ -0,0 +1,28 @@
+export default [
+  {
+    avatarIcon: '../../images/java.png',
+    userName: 'Java',
+    handle: 'java',
+  },
+  {
+    avatarIcon: '../../images/relaspace.webp',
+    userName: 'Relativity Space',
+    handle: 'relativityspace',
+  },
+  {
+    avatarIcon: '../../images/virgin.svg',
+    userName: 'Virgin Galactic',
+    handle: 'virgingalactic',
+  },
+  {
+    avatarIcon: '../../images/nasa.png',
+    userName: 'Nasa',
+    handle: 'NASA',
+  },
+  {
+    avatarIcon: '../../images/tesla.png',
+    userName: 'Tesla',
+    handle: 'Tesla',
+  },
+];
+
