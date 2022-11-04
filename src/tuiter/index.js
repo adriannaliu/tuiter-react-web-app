ded1be4d26fd826809f082c01917bb5b828b031b
adriannaliu <72051204+adriannaliu@users.noreply.github.com>
2022-11-04 13:42:00 -0400

    who to follow and post summary list components

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -3,10 +3,18 @@ import NavigationSidebar from "./navigation-sidebar";
 import WhoToFollowList
   from "./who-to-follow-list/index.js";
 import ExploreComponent from "./explore/index.js";
+import whoReducer
+  from "./reducers/who-reducer";
+import tuitsReducer from "./tuits/tuits-reducer";
+import { configureStore }
+  from '@reduxjs/toolkit';
+import {Provider} from "react-redux";
+const store = configureStore(
+    {reducer: {who: whoReducer,tuits: tuitsReducer}});
 
 function Tuiter() {
   return (
-      <>
+      <Provider store={store}>
         <Nav/>
         <div className="row mt-2">
           <div className="col-2 col-md-2 col-lg-1 col-xl-2">
@@ -20,7 +28,7 @@ function Tuiter() {
             <WhoToFollowList/>
           </div>
         </div>
-      </>
+      </Provider>
   );
 }
 
